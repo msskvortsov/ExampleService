ca1e88cd079473ba084558a12a6d0db20b7e6854
Maxim <m.skvortsov@netris.ru>
2020-12-07 12:33:40 +0300

    main: QCoreApplication -> QAndroidService

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include <QCoreApplication>
+#include <QAndroidService>
 
 #include <android/log.h>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QAndroidService a(argc, argv);
 
     __android_log_print(ANDROID_LOG_INFO, "Example", "Hello from main");
 
